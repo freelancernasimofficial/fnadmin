b92b23ff03c0f4c3c118c94b08be473c1ff9325e
freelancernasimofficial <freelancernasim@gmail.com>
2023-09-08 12:09:58 +0600

    update

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import React, { HTMLAttributes } from 'react'
 import { IoHomeOutline, IoCaretDown, IoCaretUp } from 'react-icons/io5'
+import Row from './Row'
 type Props = {
   children: React.ReactNode
 } & HTMLAttributes<HTMLDivElement>
@@ -15,8 +16,13 @@ type MenuItemProps = {
 }
 export const MenuItem = ({ href, title, Icon }: MenuItemProps) => {
   return (
-    <Link href={href} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-      {Icon ? <Icon className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" /> : null}
+    <Link
+      href={href}
+      className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+    >
+      {Icon ? (
+        <Icon className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+      ) : null}
 
       <span className="ml-3 menuItemText font-semibold">{title}</span>
     </Link>
@@ -28,16 +34,32 @@ type DropDownLabel = {
   children?: React.ReactNode
 }
 
-export const MenuDropdownLabel = ({ children, title, Icon }: DropDownLabel) => {
+export const MenuItemDropDown = ({ children, title, Icon }: DropDownLabel) => {
   return (
-    <div className="group w-full dropdownWrapper  cursor-pointer">
-      <input type="checkbox" className="scale-0 invisible w-0 h-0 hidden absolute left-[99999px] peer/dropdown top-0" id={title?.replaceAll(' ', '')} />
-      <label htmlFor={title?.replaceAll(' ', '')} className="flex cursor-pointer peer-checked/dropdown:[&>#dropdowncaretUp]:block peer-checked/dropdown:[&>#dropdowncaretdown]:hidden  rounded-lg  group-hover:bg-gray-100  dark:group-hover:bg-gray-700">
-        <div className="w-8 h-8">{Icon ? <Icon className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" /> : null}</div>
-        <div>
-          <span className="flex-1 text-gray-950 dark:text-white  font-semibold ml-3 text-left whitespace-nowrap">{title}</span>
-          <IoCaretDown id="dropdowncaretdown" />
-          <IoCaretUp id="dropdowncaretUp" className="hidden" />
+    <div className="w-full MenuItemDropDown  cursor-pointer">
+      <input
+        type="checkbox"
+        className="scale-0 invisible w-0 h-0 hidden absolute left-[99999px] peer/dropdown top-0"
+        id={title?.replaceAll(' ', '')}
+      />
+      <label
+        htmlFor={title?.replaceAll(' ', '')}
+        className="flex group flex-row py-1.5  w-full items-center justify-between   cursor-pointer peer-checked/dropdown:[&>span>#dropdowncaretUp]:!inline peer-checked/dropdown:[&>span>#dropdowncaretdown]:!hidden  rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800"
+      >
+        {Icon ? (
+          <Icon className="flex-shrink-0 mx-2.5 w-5 h-5 text-gray-500 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" />
+        ) : null}
+
+        <div className="flex min-w-[200px] flex-1  flex-row items-center justify-between  text-gray-950 dark:text-white  font-semibold text-left whitespace-nowrap">
+          <span> {title}</span>{' '}
+          <IoCaretDown
+            className="dark:text-gray-400 mr-2  inline group-hover:text-gray-950 dark:group-hover:text-white text-gray-500"
+            id="dropdowncaretdown"
+          />
+          <IoCaretUp
+            className="dark:text-gray-400 mr-2  dark:group-hover:text-white group-hover:text-gray-950 text-gray-500 hidden"
+            id="dropdowncaretUp"
+          />
         </div>
       </label>
 
